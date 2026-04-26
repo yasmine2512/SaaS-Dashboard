f909e5305c9e5bc10f58e47e2ccc50c08534cc0e
Yasmine <yasmineseddari32@gmail.com>
2026-04-26 21:30:23 +0100

    order api

--- server/Routes/ordersRoutes.js
+++ server/Routes/ordersRoutes.js
@@ -1,26 +1,44 @@
 import express from "express";
+import Order from "../Models/Order.js";
+// import Product from "../Models/Product";
+import asyncHandler from "express-async-handler";
+import { verifyTokenAndAuthorization } from "../Middlewares/JWTauth.js";
 const router = express.Router();
 export default router; 
 
 /** 
    * @desc show all orders 
-   * @route /api/auth/register
-   * @method POST
+   * @route /api/orders/:id
+   * @method GET
    * @access private
    */  
+router.get("/:id",verifyTokenAndAuthorization,asyncHandler(async(req,res)=>{
+   const userId = req.params.id;
+const userOrders = await constOrder.find({_id : userId});
+return res.json(userOrders);
 
+}))
 
 
 
 /** 
    * @desc create an order
-   * @route /api/auth/register
+   * @route /api/orders/:id
    * @method POST
    * @access private
    */  
+router.post("/:id",verifyTokenAndAuthorization,asyncHandler(async(req,res)=>{
+   const userId = req.params.id;
+   const {products,totalprice} = req.body;
+   const order = new Order({
+       product: products,
+       totalPrice: totalprice,
+   })
+   await order.save();
+   return res.status(201).json({massage:"order created"});
 
-
-
+}))
+ 
 
 
 
