1a8574b1431e45a9f01f6181d36507aa24d18d5f
Yasmine <yasmineseddari32@gmail.com>
2026-04-27 23:44:35 +0100

    to test later

--- server/Routes/ordersRoutes.js
+++ server/Routes/ordersRoutes.js
@@ -1,25 +1,24 @@
 import express from "express";
 import Order from "../Models/Order.js";
+import User from "../Models/User.js";
 // import Product from "../Models/Product";
 import asyncHandler from "express-async-handler";
-import { verifyTokenAndAuthorization } from "../Middlewares/JWTauth.js";
+import { verifyTokenAndAdmin, verifyTokenAndAuthorization } from "../Middlewares/JWTauth.js";
 const router = express.Router();
 export default router; 
 
 /** 
    * @desc show all orders 
-   * @route /api/orders/:id
+   * @route /api/orders/user/:id
    * @method GET
    * @access private
    */  
-router.get("/:id",verifyTokenAndAuthorization,asyncHandler(async(req,res)=>{
+router.get("/user/:id",verifyTokenAndAuthorization,asyncHandler(async(req,res)=>{
    const userId = req.params.id;
-const userOrders = await constOrder.find({_id : userId});
+const userOrders = await Order.find({_id : userId});
 return res.json(userOrders);
 
 }))
-
-
 
 /** 
    * @desc create an order
@@ -30,33 +29,49 @@ return res.json(userOrders);
 router.post("/:id",verifyTokenAndAuthorization,asyncHandler(async(req,res)=>{
    const userId = req.params.id;
    const {products,totalprice} = req.body;
+   const user = await User.findById(userId);
    const order = new Order({
-       product: products,
+       user,
+       products: products,
        totalPrice: totalprice,
    })
    await order.save();
-   return res.status(201).json({massage:"order created"});
+   return res.status(201).json({message:"order created"});
 
 }))
  
-
-
-
 /** 
    * @desc check the order
-   * @route /api/auth/register
-   * @method POST
+   * @route /api/orders/completed/:id
+   * @method PUT
    * @access private
    */  
-
-
-
-
-
-
+router.put("/completed/:id",verifyTokenAndAdmin,asyncHandler(async(req,res)=>{
+   const orderId = req.params.id;
+   const order = await Order.findByIdAndUpdate(orderId,{
+      $set:{status: "completed",}
+   },{new : true});
+   await order.save().then(result =>{
+      res.json({message : "updated to completed"}).catch(err =>{
+         console.log(err);
+      })
+   })
+}))
+ 
 /** 
    * @desc deliver order
-   * @route /api/auth/register
+   * @route /api/orders/delivered/:id
    * @method POST
    * @access private
    */  
+  router.put("/delivered/:id",verifyTokenAndAdmin,asyncHandler(async(req,res)=>{
+   const orderId = req.params.id;
+   const order = await Order.findByIdAndUpdate(orderId,{
+      $set:{status: "delivred",}
+   },{new : true});
+   await order.save().then(result =>{
+      res.json({message : "updated to delivered"});
+   }).catch(err =>{
+         console.log(err);
+      })
+}))
