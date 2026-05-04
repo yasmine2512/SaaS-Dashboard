9955940e9d97a9abeb55007abee7935e712a9a2c
Yasmine <yasmineseddari32@gmail.com>
2026-05-04 23:43:58 +0100

    fixed product form

--- client/src/Pages/AddProduct.jsx
+++ client/src/Pages/AddProduct.jsx
@@ -54,86 +54,110 @@ try{
   if (!open) return null
 
   return (
-    <div className="fixed inset-0 bg-black/40 flex items-center justify-center z-50">
-        {/* max-h-[80vh] */}
-      <div className="bg-white w-[600px] h-[90vh] rounded-2xl overflow-y-auto shadow-xl p-8 flex flex-col border">
+<div className="fixed inset-0 bg-black/40 flex items-center justify-center z-50">
+  <div className="bg-white w-[700px] h-[92vh] rounded-2xl overflow-y-auto shadow-xl p-6 flex flex-col border">
 
-        <h2 className="text-xl font-semibold mb-4">
-          Add Product
-        </h2>
+    <h2 className="text-xl font-semibold mb-5 ">
+      Add Product
+    </h2>
 
-        <div className="space-y-3">
-          <input
-            name="name"
-            placeholder="Product name"
-            className="w-full border p-2 rounded-lg"
-            onChange={handleChange}
-          />
+    <div className="grid grid-cols-2 gap-2">
 
-          <input
-            name="price"
-            placeholder="Price (ex: $49/mo)"
-            className="w-full border p-2 rounded-lg"
-            onChange={handleChange}
-          />
+      <div className="flex flex-col mx-1">
+        <label className="mb-2 text-left  ml-3">Product Name</label>
+        <input
+          name="name"
+          placeholder="Product name"
+          className="border p-2 rounded-lg w-full "
+          onChange={handleChange}
+        />
+      </div>
 
-          <input
-            name="category"
-            placeholder="Category"
-            className="w-full border p-2 rounded-lg"
-            onChange={handleChange}
-          />
+      <div className="flex flex-col mx-1">
+        <label className="mb-2 text-left ml-3">Price</label>
+        <input
+          name="price"
+          placeholder="Price (ex: $49/mo)"
+          className="border p-2 rounded-lg w-full"
+          onChange={handleChange}
+        />
+      </div>
 
-          <input
-            name="stock"
-            placeholder="Stock"
-            className="w-full border p-2 rounded-lg"
-            onChange={handleChange}
-          />
+      <div className="flex flex-col mx-1">
+        <label className="mb-2 text-left ml-3">Category</label>
+        <input
+          name="category"
+          placeholder="Category"
+          className="border p-2 rounded-lg w-dull"
+          onChange={handleChange}
+        />
+      </div>
 
-          <textarea
-            name="description"
-            placeholder="Description"
-            className="w-full border p-2 rounded-lg"
-            rows={3}
-            onChange={handleChange}
-          />
 
-          <textarea
-            name="features"
-            placeholder="Features (one per line)"
-            className="w-full border p-2 rounded-lg"
-            rows={4}
-            onChange={handleChange}
-          />
+      <div className="flex flex-col mx-1">
+        <label className="mb-2 text-left ml-3">Stock</label>
+        <input
+          name="stock"
+          placeholder="Stock"
+          className="border p-2 rounded-lg w-full"
+          onChange={handleChange}
+        />
+      </div>
 
-          <input
+      <div className="flex flex-col mx-1">
+        <label className="mb-2 text-left ml-3">Description</label>
+        <textarea
+          name="description"
+          placeholder="Description"
+          className="border p-2 rounded-lg w-full"
+          rows={3}
+          onChange={handleChange}
+        />
+      </div>
+
+      <div className="flex flex-col mx-1">
+        <label className="mb-2 text-left ml-3">Features</label>
+        <textarea
+          name="features"
+          placeholder="Features (one per line)"
+          className="border p-2 rounded-lg w-full"
+          rows={3}
+          onChange={handleChange}
+        />
+      </div>
+
+      <div className="flex flex-col col-span-2 mx-1">
+        <label className="mb-2 text-left ml-3">Image</label>
+        <input
           type="file"
           name="image"
           accept="image/*"
           onChange={(e) =>
-          setForm({ ...form, image: e.target.files[0] })}
-          className="w-full border p-2 rounded-lg"
-          />
-
-        </div>
-
-        <div className="flex justify-end gap-3 mt-5">
-          <button
-            onClick={() => setOpen(false)}
-            className="px-4 py-2 border rounded-lg"
-          >
-            Cancel
-          </button>
-
-          <button
-            onClick={handleSubmit}
-            className="px-4 py-2 bg-black text-white rounded-lg"
-          >
-            Add Product
-          </button>
-        </div>
+            setForm({ ...form, image: e.target.files[0] })
+          }
+          className="border p-2 rounded-lg"
+        />
       </div>
+
     </div>
+
+    <div className="flex justify-end gap-3 mt-6">
+      <button
+        onClick={() => setOpen(false)}
+        className="px-4 py-2 border rounded-lg"
+      >
+        Cancel
+      </button>
+
+      <button
+        onClick={handleSubmit}
+        className="px-4 py-2 bg-black text-white rounded-lg"
+      >
+        Add Product
+      </button>
+    </div>
+
+  </div>
+</div>
   )
 }
